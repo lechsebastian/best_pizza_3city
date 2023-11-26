6dc1feac135f8965929e2b4b6fa6dad94ab551b4
Sebastian Lech <lechsebastian99@gmail.com>
2023-11-26 01:44:33 +0100

    Add BottomNavigationBar to HomePage

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({
     super.key,
     required this.user,
@@ -10,10 +10,66 @@ class HomePage extends StatelessWidget {
   final User user;
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var currentIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Jesteś zalogowany jako ${user.email}'),
+      body: Builder(builder: (context) {
+        if (currentIndex == 0) {
+          return const Scaffold(
+            body: Center(
+              child: Text('Jeden'),
+            ),
+          );
+        } else if (currentIndex == 1) {
+          return const Scaffold(
+            body: Center(
+              child: Text('Dwa'),
+            ),
+          );
+        }
+        return Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Jesteś zalogowany jako ${widget.user.email}'),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  FirebaseAuth.instance.signOut();
+                },
+                child: const Text('Log out'),
+              ),
+            ],
+          ),
+        );
+      }),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (newIndex) {
+          setState(() {
+            currentIndex = newIndex;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: 'Reviews',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'Add',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'My account',
+          ),
+        ],
       ),
     );
   }
