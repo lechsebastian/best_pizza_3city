7bca1fab38d4252889d179e8fefcbcd2d8d9291b
Sebastian Lech <lechsebastian99@gmail.com>
2023-12-03 01:17:16 +0100

    Implement RootCubit

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,7 +1,8 @@
+import 'package:best_pizza_3city/app/cubit/root_cubit.dart';
 import 'package:best_pizza_3city/app/features/home/home_page.dart';
 import 'package:best_pizza_3city/app/features/login/login_page.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -26,16 +27,17 @@ class RootPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<User?>(
-      stream: FirebaseAuth.instance.authStateChanges(),
-      builder: (context, snapshot) {
-        final user = snapshot.data;
-
-        if (user == null) {
-          return LoginPage();
-        }
-        return HomePage(user: user);
-      },
+    return BlocProvider(
+      create: (context) => RootCubit()..start(),
+      child: BlocBuilder<RootCubit, RootState>(
+        builder: (context, state) {
+          final user = state.user;
+          if (user == null) {
+            return LoginPage();
+          }
+          return HomePage(user: user);
+        },
+      ),
     );
   }
 }
